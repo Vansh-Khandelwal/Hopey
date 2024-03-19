9186143573de68defb667c3b27fda4d89235c868
Vansh <vansh.khandelwal06@gmail.com>
2024-03-19 12:47:29 +0530

    Deploy process 2

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -10,14 +10,15 @@ import './Home.scss';
 
 const Home = () => {
 
-  const content1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corporis enim minima recusandae laudantium? Quidem, odit voluptatum quia labore incidunt vero perspiciatis doloribus perferendis, nostrum doloremque repellendus a obcaecati ea.";
+  const featuredContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corporis enim minima recusandae laudantium? Quidem, odit voluptatum quia labore incidunt vero perspiciatis doloribus perferendis, nostrum doloremque repellendus a obcaecati ea.(Dummy content 1)";
+  const trendContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus corporis enim minima recusandae laudantium? Quidem, odit voluptatum quia labore incidunt vero perspiciatis doloribus perferendis, nostrum doloremque repellendus a obcaecati ea.(Dummy content 2)";
 
   return (
     <div className="home">
       <Slider/>
-      <Featured type="Featured" content={content1}/>
+      <Featured type="Featured" content={featuredContent} />
       <Categories/>
-      <Featured type="Trending" content={content1}/>
+      <Featured type="Trending" content={trendContent} />
       <Contact/>
     </div>
   )
