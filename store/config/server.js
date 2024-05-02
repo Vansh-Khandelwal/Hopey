0eec62bd6b5111d6e96a99eda985aeaa7ecc9a31
Vansh <vansh.khandelwal06@gmail.com>
2024-05-02 16:57:19 +0530

    Deploy

--- store/config/server.js
+++ store/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    // port: env.int('PORT', 1337),
     url: env('PUBLIC_URL', 'https://hopey.onrender.com'),
     proxy: env.bool('IS_PROXIED', true),
     app: {
