7a8d4a4a146006abc0dbbca45f33e0bb2a7152da
Vansh <vansh.khandelwal06@gmail.com>
2024-05-02 19:31:09 +0530

    Deploy

--- store/config/server.js
+++ store/config/server.js
@@ -6,6 +6,8 @@ module.exports = ({ env }) => ({
     app: {
         keys: env.array('APP_KEYS'),
     },
+    cron: { enabled: true },
+    cors: { enabled: true, origin: ['*'] },
     webhooks: {
         populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
     },
